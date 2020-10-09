2ff58d89df36a0bf57fedf43d7bab0eb0eadc5fc
EgonOlsen <info@jpct.net>
2020-10-09 10:21:19 +0200

    Renamed directory

--- build/compile.cmd
+++ build/compile.cmd
@@ -1,3 +1,3 @@
-call mospeed ..\adv\xam.bas -compactlevel=3
-call mospeed ..\adv\brotquest.bas -compactlevel=5
-call mosm ..\adv\raster.asm
+call mospeed ..\basic\xam.bas -compactlevel=3
+call mospeed ..\basic\brotquest.bas -compactlevel=5
+call mosm ..\basic\raster.asm
