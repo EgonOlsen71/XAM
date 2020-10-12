a8fb8984f46cda6fa0112194cca5b9f72a1f2220
EgonOlsen <info@jpct.net>
2020-10-12 10:53:34 +0200

    Made image name TheC64 compatible

--- build/build.cmd
+++ build/build.cmd
@@ -1,9 +1,9 @@
-del brotquest.d64
-c1541 -format brotquest,bq d64 brotquest.d64
+del brotquest_AD.d64
+c1541 -format brotquest,bq d64 brotquest_AD.d64
 call compile.cmd
-call ..\build\c1541 ..\build\brotquest.d64 -write ++brotquest.prg brotquest,p
-call ..\build\c1541 ..\build\brotquest.d64 -write ++xam.prg xam,p
-call ..\build\c1541 ..\build\brotquest.d64 -write raster.prg xam2,p
+call ..\build\c1541 ..\build\brotquest_AD.d64 -write ++brotquest.prg brotquest,p
+call ..\build\c1541 ..\build\brotquest_AD.d64 -write ++xam.prg xam,p
+call ..\build\c1541 ..\build\brotquest_AD.d64 -write raster.prg xam2,p
 cd ..\build
 cd ..\seq
 for %%f in (*.*) do call :add "%%f"
@@ -11,4 +11,4 @@ cd ..\build
 goto :EOF
 
 :add
-..\build\c1541 ..\build\brotquest.d64 -write %1 %1,s
+..\build\c1541 ..\build\brotquest_AD.d64 -write %1 %1,s
