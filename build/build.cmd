93cc050df5e2fb6ccf04bae9721f52ae91da96ce
EgonOlsen <info@jpct.net>
2020-10-16 21:41:51 +0200

    Builds with compression enabled

--- build/build.cmd
+++ build/build.cmd
@@ -2,7 +2,7 @@ del brotquest_AD.d64
 c1541 -format brotquest,bq d64 brotquest_AD.d64
 call compile.cmd
 call ..\build\c1541 ..\build\brotquest_AD.d64 -write ++brotquest.prg brotquest,p
-call ..\build\c1541 ..\build\brotquest_AD.d64 -write ++xam.prg xam,p
+call ..\build\c1541 ..\build\brotquest_AD.d64 -write ++xam-c.prg xam,p
 call ..\build\c1541 ..\build\brotquest_AD.d64 -write raster.prg xam2,p
 cd ..\build
 cd ..\seq
