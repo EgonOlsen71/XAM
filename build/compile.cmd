1c0467e0e62bb593cde9fb6c4713dec358522013
EgonOlsen <info@jpct.net>
2022-04-14 17:52:33 +0200

    New release

--- build/compile.cmd
+++ build/compile.cmd
@@ -1,3 +1,3 @@
-call mospeed ..\basic\xam.bas -compactlevel=3 -compression=true
+call mospeed ..\basic\xam.bas -compactlevel=3
 call mospeed ..\basic\brotquest.bas -compactlevel=5
 call mosm ..\basic\raster.asm
